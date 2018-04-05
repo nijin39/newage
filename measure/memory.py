63987f6a9760f0a9ae314a2e511343d4adc36512
parksungsu <tempprgdanny90@gmail.com>
2018-04-05 17:02:30 +0900

    finally

--- measure/memory.py
+++ measure/memory.py
@@ -3,104 +3,45 @@ from flask_restplus import Api, Resource, fields
 import json
 import psutil
 
-
 api_v1_memory = Blueprint('api_v1_memory', __name__, url_prefix='/api/1/memory')
-
-api = Api(api_v1_memory, version='1.0', title='Todo API',
-          description='A simple TODO API',
+api = Api(api_v1_memory, version='1.0', title='Memory API',
+          description='Getting Memory Infomation API',
           )
-
-ns = api.namespace('todos', description='TODO operations')
-
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
-
-todo = api.model('Todo', {
-    'task': fields.String(required=True, description='The task details')
-})
-
-listed_todo = api.model('ListedTodo', {
-    'id': fields.String(required=True, description='The todo ID'),
-    'todo': fields.Nested(todo, description='The Todo')
-})
-
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        api.abort(404, "Todo {} doesn't exist".format(todo_id))
 
 parser = api.parser()
 parser.add_argument('task', type=str, required=True, help='The task details', location='form')
 
-
-@ns.route('/<string:todo_id>')
-@api.doc(responses={404: 'Todo not found'}, params={'todo_id': 'The Todo ID'})
-class Todo(Resource):
-    '''Show a single todo item and lets you delete them'''
-    @api.doc(description='todo_id should be in {0}'.format(', '.join(TODOS.keys())))
-    @api.marshal_with(todo)
-    def get(self, todo_id):
-        '''Fetch a given resource'''
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    @api.doc(responses={204: 'Todo deleted'})
-    def delete(self, todo_id):
-        '''Delete a given resource'''
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    @api.doc(parser=parser)
-    @api.marshal_with(todo)
-    def put(self, todo_id):
-        '''Update a given resource'''
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task
-
-''' /memory/virtual_memory'''
-
-
-@ns.route('/virtualMemory')
+@api.route('/virtualMemory')
 class VirtualMemory(Resource):
     def get(self):
-        '''Get Virtual Memory'''
-        vm = psutil.virtual_memory()
-        print(type(vm))
-        #sm = psutil.swap_memory()
+        '''Get Virtual Memory
+        Success : json of Virtual memory infomation
+        Fail : Err Virtual memory
+        '''
+        str = ''
+        try:
+            str = json.dumps(psutil.virtual_memory()._asdict())
+        except:
+            str = 'Err Virtual Memory'
+        finally:
+            return str
 
-        # svmem(total=10367352832, available=6472179712, percent=37.6,
-        # used=8186245120, free=2181107712, active=4748992512, inactive=2758115328,
-        # buffers=790724608, cached=3500347392, shared=787554304)
-
-        #dict = {'virtualMemory':vm}
-        return [{'id': id, 'todo': todo} for id, todo in TODOS.items()]
-
-''' /memory/swap_memory'''
-@ns.route('/')
-class TodoList(Resource):
-    '''Shows a list of all todos, and lets you POST to add new tasks'''
-    @api.marshal_list_with(listed_todo)
+@api.route('/swapMemory')
+class SwapMemory(Resource):
     def get(self):
-        '''List all todos'''
-        return [{'id': id, 'todo': todo} for id, todo in TODOS.items()]
-
-    @api.doc(parser=parser)
-    @api.marshal_with(todo, code=201)
-    def post(self):
-        '''Create a todo'''
-        args = parser.parse_args()
-        todo_id = 'todo%d' % (len(TODOS) + 1)
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-
+        '''Get Swap Memory
+        Success : json of Swap memory infomation
+        Fail : Err Swap memory
+        '''
+        str = ''
+        try:
+            str = json.dumps(psutil.swap_memory()._asdict())
+        except:
+            str = 'Err Swap Memory'
+        finally:
+            return str
 
 if __name__ == '__main__':
     app = Flask(__name__)
-    app.register_blueprint(api_v1)
+    app.register_blueprint(api_v1_memory)
     app.run(debug=True)
