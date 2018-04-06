ebe7753ebd29d64936ab5feb27ea5321916b0a65
kim jong il <nijin@gmail.com>
2018-04-06 10:53:07 +0900

    Modify disk

--- measure/disk.py
+++ measure/disk.py
@@ -1,7 +1,6 @@
 from flask import Flask, Blueprint, jsonify
 from flask_restplus import Api, Resource, fields
-import disk_service
-
+import disk_service as service
 
 api_v1_disk = Blueprint('api_v1_disk', __name__, url_prefix='/api/1/disk')
 
@@ -9,20 +8,29 @@ api = Api(api_v1_disk, version='1.0', title='Disk Information', description='Sho
 
 ns = api.namespace('disk', description='Disk Information')
 
+parser = api.parser()
+parser.add_argument('task', type=str, required=True, help='The task details', location='form')
+
+
+
 @ns.route('/partitions')
 class diskPartitions(Resource):
+
     def get(self):
-        return jsonify(disk_service.part())
+        return jsonify(service.part())
+
 
 @ns.route('/usage')
 class diskUsage(Resource):
+
     def get(self):
-        return jsonify(disk_service.usage())
+        return jsonify(service.usage())
 
 @ns.route('/ioCounters')
 class diskIoCounters(Resource):
+
     def get(self):
-        return jsonify(disk_service.count())
+        return jsonify(service.count())
 
 if __name__ == '__main__':
     app = Flask(__name__)
